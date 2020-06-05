47c5ee86541345cee693d33e826997db9820352b
gustavo2892 <gustavo2892@gmail.com>
2020-06-05 18:21:12 -0300

    Dashboard Page

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,5 +2,10 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
+
+  @media (max-width: 1400px) {
+    height: 110%;
+  }
+
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
 `;
